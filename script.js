c81286c4a5bf05fe21932c354e1931d7b91cdd2b
tevistumulty <tevis.tumulty@west-mec.org>
2017-12-18 11:36:44 -0700

    Initial upload

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+
+function getQueryVariableEsc(variable) {
+    var query = window.location.search.substring(1);
+    //alert(query);
+	var vars = query.split("&amp;");
+	//alert(vars);
+    for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+        //alert(pair);
+		if (decodeURIComponent(pair[0]) == variable) {
+			return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
