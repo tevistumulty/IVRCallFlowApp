cabe7d5a07ea6d063ac3ac6e2b96a27321f18700
tevistumulty <tevis.tumulty@west-mec.org>
2017-12-19 13:15:07 -0700

    Code cleanup and commenting

--- script.js
+++ script.js
@@ -7,27 +7,24 @@ function getQueryVariable(variable) {
         var pair = vars[i].split('=');
         if (decodeURIComponent(pair[0]) == variable) {
 			return decodeURIComponent(pair[1]);
-        }
-    }
+        }//end if
+    }//end for
     console.log('Query variable %s not found', variable);
-}
+}//end getQueryVariable
 
 function getQueryVariableEsc(variable) {
     var query = window.location.search.substring(1);
-    //alert(query);
 	var vars = query.split("&amp;");
-	//alert(vars);
     for (var i = 0; i < vars.length; i++) {
 		var pair = vars[i].split('=');
-        //alert(pair);
 		if (decodeURIComponent(pair[0]) == variable) {
 			return decodeURIComponent(pair[1]);
-        }
-    }
+        }//end if
+    }//end for
     console.log('Query variable %s not found', variable);
-}
+}//end getQueryVariableEsc
 
 function printAudit(text) {
-	var newString = document.getElementById("txtAudit").value + text + "|";
+	var newString = document.getElementById("txtAudit").value.replace(/\n/g, "|") + text + "|";
 	document.cookie = "audit=" + newString;
-}
+}//end printAudit
